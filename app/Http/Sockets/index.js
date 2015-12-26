45124a469a05a5093f8709bd79e9adb8a3e1073e
unknown <sipimokus@gmail.com>
2015-12-26 02:08:38 +0100

    Ready for express-go v0.3

--- app/Http/Sockets/index.js
+++ app/Http/Sockets/index.js
@@ -1,14 +1,15 @@
-// Include the cluster module
-var cluster = require('cluster');
+var cluster  = require( 'cluster' );
 
-module.exports.socket = function (io)
+module.exports.socket = function ( io )
 {
     // io stuff here... io.on('conection.....
     io.on('connection', function (socket)
     {
         socket.emit('news', { hello: 'worker: ' + cluster.worker.id });
-        socket.on('my other event', function (data) {
+        socket.on('my other event', function (data)
+        {
             console.log(data);
         });
     });
+
 };
---
+++ bootstrap/Loaders/Foo.js
@@ -0,0 +1,95 @@
+/**
+ * Sample "foo" loader
+ */
+
+module.exports.loader = function( app )
+{
+    return {
+
+        /**
+         * Prefix used name for components
+         * Ex.: module.exports.prefix = {};
+         *
+         * Use "null" for disable
+         *
+         * YOU CAN OVERRIDE HERE CORE PROVIDER PREFIX!
+         * Ex.: config
+         *
+         * @returns {string}
+         */
+        exportName : function()
+        {
+            return 'foo';
+        },
+
+
+        /**
+         * Load object into global namespace
+         *
+         * Use "false" for disable
+         *
+         * @returns {boolean}
+         */
+        exportNamespace : function()
+        {
+            return true;
+        },
+
+
+        /**
+         * Manual or automatic booting
+         * Default, if not defined: false [automatic]
+         *
+         * Use: app.boot("Foo")
+         *
+         * @returns {boolean}
+         */
+        manualBoot : function()
+        {
+            return false;
+        },
+
+
+        /**
+         * Register method
+         *
+         * @param loadObject
+         * @param nameObject
+         * @returns any
+         */
+        register : function( loadObject, nameObject )
+        {
+        },
+
+
+        /**
+         * Boot method
+         *
+         * @param app
+         * @returns void
+         */
+        boot : function( app )
+        {
+        },
+
+
+        /**
+         * Loader method
+         *
+         * You can override default object initialization method
+         *
+         * @param loadObject
+         * @param nameObject
+         * @returns {any}
+         */
+        loader : function( loadObject, nameObject )
+        {
+            // Use default method
+            return null;
+
+            // The default method
+            // return loadObject( app );
+        }
+
+    };
+};
